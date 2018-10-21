018b7fb544c3821c71b51b4032bfe29e83a211dd
yiyank <yiyank@umich.edu>
2018-10-20 23:25:04 -0400

    Blank Activity Structure - 8 Pages
    1. FirstPage
    2. LoginPage
    3. Map
    4. ConferenceIntro
    5. Schedule
    6. SurveyPage
    7. ProfileList
    8. SmartProfile

---
+++ app/src/main/java/com/example/to426/dayofgoc/ConferenceIntro.java
@@ -0,0 +1,13 @@
+package com.example.to426.dayofgoc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConferenceIntro extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conference_intro);
+    }
+}
---
+++ app/src/main/java/com/example/to426/dayofgoc/Schedule.java
@@ -0,0 +1,13 @@
+package com.example.to426.dayofgoc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Schedule extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
