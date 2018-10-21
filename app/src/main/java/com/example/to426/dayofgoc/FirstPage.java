3d32148f22a959a0fd8e45d91b6519de5f040356
yiyank <yiyank@umich.edu>
2018-10-20 23:16:28 -0400

    Initial commit

---
+++ app/src/main/java/com/example/to426/dayofgoc/FirstPage.java
@@ -0,0 +1,15 @@
+package com.example.to426.dayofgoc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+    }
+
+
+}
