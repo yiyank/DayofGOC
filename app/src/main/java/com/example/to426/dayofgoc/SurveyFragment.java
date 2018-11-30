634df6b62a6bc7d3fec400fef2ec8511e20e42e7
Jessie <jessyang@umich.edu>
2018-11-30 11:48:51 -0500

    Added Bottom Nav

---
+++ app/src/main/java/com/example/to426/dayofgoc/SurveyFragment.java
@@ -0,0 +1,29 @@
+package com.example.to426.dayofgoc;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SurveyFragment extends Fragment {
+
+
+    public SurveyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_survey, container, false);
+    }
+
+}
